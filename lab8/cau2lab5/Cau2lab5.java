aaa84c69e4676cc304659ba3041c6dd76e43dd42
ptheanh412 <pham041203theanh@gmail.com>
2023-04-14 00:36:29 +0700

    a

---
+++ lab8/cau2lab5/Cau2lab5.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cau2lab5;
+
+/**
+ *
+ * @author 84974
+ */
+public class Cau2lab5 {
+
+    
+    public static void main(String[] args) {
+        DanhSach b = new DanhSach();
+        b.menu();
+    }
+    
+}
---
+++ lab8/cau3lab5/Cau3lab5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cau3lab5;
+
+/**
+ *
+ * @author 84974
+ */
+public class Cau3lab5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DanhSach d = new DanhSach();
+        d.menu();
+    }
+    
+}
